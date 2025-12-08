671d6a4c96b916f6e006b13d93140b263a82d3e1
Michael Pham <michael.pham@aveo-global.com>
2025-12-08 14:33:58 +0700

    WordPress.Security.EscapeOutput.UnsafePrintingFunction tab galleries

--- includes/views/view-migrate-tab-galleries.php
+++ includes/views/view-migrate-tab-galleries.php
@@ -22,7 +22,7 @@
                 success: success_callback,
                 error: function(xhr, ajaxOptions, thrownError) {
                     //something went wrong! Alert the user and reload the page
-                    alert('<?php _e( 'Something went wrong with the migration and the page will now reload. Once it has reloaded, click "Resume Migration" to continue with the migration.', 'foogallery-migrate' ); ?>');
+                    alert('<?php echo esc_js( __( 'Something went wrong with the migration and the page will now reload. Once it has reloaded, click "Resume Migration" to continue with the migration.', 'foogallery-migrate' ) ); ?>');
                     location.reload();
                 }
             });
@@ -60,7 +60,7 @@
 
         $form.on('click', '.cancel_migrate', function (e) {
             e.preventDefault();
-            if (!confirm('<?php _e( 'Are you sure you want to cancel?', 'foogallery-migrate' ); ?>')) {
+            if (!confirm('<?php echo esc_js( __( 'Are you sure you want to cancel?', 'foogallery-migrate' ) ); ?>')) {
                 return false;
             } else {
                 foogallery_gallery_migration_ajax( 'foogallery_migrate_cancel', function (data) {
