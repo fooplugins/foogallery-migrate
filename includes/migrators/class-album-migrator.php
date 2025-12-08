cb234f86cd5ea14180435a28fa64afb1556e87f3
Michael Pham <michael.pham@aveo-global.com>
2025-12-08 14:31:59 +0700

    WordPress.Security.EscapeOutput.UnsafePrintingFunction migrator album

--- includes/migrators/class-album-migrator.php
+++ includes/migrators/class-album-migrator.php
@@ -30,7 +30,7 @@ if ( ! class_exists( 'FooPlugins\FooGalleryMigrate\Migrators\AlbumMigrator' ) ) 
             wp_nonce_field( 'foogallery_album_migrate', 'foogallery_album_migrate', false );
 
             if ( count( $albums ) === 0 ) {
-                echo '<p>' . __( 'No albums found!', 'foogallery-migrate' ) . '</p>';
+                echo '<p>' . esc_html__( 'No albums found!', 'foogallery-migrate' ) . '</p>';
                 $show_refresh = true;
                 $migrating = false;
             } else {
@@ -57,24 +57,24 @@ if ( ! class_exists( 'FooPlugins\FooGalleryMigrate\Migrators\AlbumMigrator' ) ) 
                         <tr>
                             <td id="cb" class="manage-column column-cb check-column">
                                 <?php if ( ! $migrating ) { ?>
-                                    <label class="screen-reader-text" for="cb-select-all-1"><?php _e( 'Select All', 'foogallery-migrate' ); ?></label>
+                                    <label class="screen-reader-text" for="cb-select-all-1"><?php esc_html_e( 'Select All', 'foogallery-migrate' ); ?></label>
                                     <input id="cb-select-all-1" type="checkbox" <?php echo $migrating ? 'disabled="disabled"' : ''; ?> checked="checked" />
                                 <?php } ?>
                             </td>
                             <th scope="col" class="manage-column">
-                                <span><?php _e( 'Album', 'foogallery-migrate' ); ?></span>
+                                <span><?php esc_html_e( 'Album', 'foogallery-migrate' ); ?></span>
                             </th>
                             <th scope="col" class="manage-column">
-                                <span><?php _e( 'Source', 'foogallery-migrate' ); ?></span>
+                                <span><?php esc_html_e( 'Source', 'foogallery-migrate' ); ?></span>
                             </th>
                             <th scope="col" class="manage-column">
-                                <span><?php _e( 'Migration Data', 'foogallery-migrate' ); ?></span>
+                                <span><?php esc_html_e( 'Migration Data', 'foogallery-migrate' ); ?></span>
                             </th>
                             <th scope="col" class="manage-column">
-                                <span><?php printf( __( '%s Album Name', 'foogallery-migrate' ), foogallery_plugin_name() ); ?></span>
+                                <span><?php printf( esc_html__( '%s Album Name', 'foogallery-migrate' ), esc_html( foogallery_plugin_name() ) ); ?></span>
                             </th>
                             <th scope="col" class="manage-column">
-                                <span><?php _e( 'Migration Progress', 'foogallery' ); ?></span>
+                                <span><?php esc_html_e( 'Migration Progress', 'foogallery' ); ?></span>
                             </th>
                         </tr>
                     </thead>
@@ -114,7 +114,7 @@ if ( ! class_exists( 'FooPlugins\FooGalleryMigrate\Migrators\AlbumMigrator' ) ) 
                         $album = $albums[$counter];
                         $progress    = $album->progress;
                         $done        = $album->migrated;
-                        $edit_link	 = '';
+                        $edit_link   = '';
                         $fooalbum = false;
                         if ( $album->migrated_id > 0 ) {
                             $fooalbum = \FooGalleryAlbum::get_by_id( $album->migrated_id );
@@ -145,16 +145,16 @@ if ( ! class_exists( 'FooPlugins\FooGalleryMigrate\Migrators\AlbumMigrator' ) ) 
                                 <?php echo esc_html( $album->plugin->name() ); ?>
                             </td>
                             <td>
-                                <?php _e( 'Galleries : ', 'foogallery-migrate' ); ?>
+                                <?php esc_html_e( 'Galleries : ', 'foogallery-migrate' ); ?>
                                 <?php echo esc_html( $album->get_children_count() ); ?>
                                 <br />
-                                <?php _e( 'Images : ', 'foogallery-migrate' ); ?>
+                                <?php esc_html_e( 'Images : ', 'foogallery-migrate' ); ?>
                                 <?php echo esc_html( $album->get_total_images() ); ?>
                             </td>
                             <td>
                                 <?php
                                  if ( $fooalbum ) {
-                                    echo $edit_link;
+                                    echo wp_kses_post( $edit_link );
                                 } else {
                                     ?>
                                     <input name="foogallery-album-title-<?php echo esc_attr( $album->unique_identifier() ); ?>" value="<?php echo esc_attr( $album->title ); ?>">
@@ -173,7 +173,7 @@ if ( ! class_exists( 'FooPlugins\FooGalleryMigrate\Migrators\AlbumMigrator' ) ) 
                 </table>
                 <div class="tablenav bottom">
                     <div class="tablenav-pages">
-                        <?php echo $pagination->render(); ?>
+                        <?php echo wp_kses_post( $pagination->render( false ) ); ?>
                     </div>
                 </div>
 
@@ -186,18 +186,18 @@ if ( ! class_exists( 'FooPlugins\FooGalleryMigrate\Migrators\AlbumMigrator' ) ) 
 
                 if ( $has_migrations ) { ?>
                     <button name="foogallery_migrate_action" value="foogallery_album_migrate_continue"
-                            class="button button-primary continue_album_migrate"><?php _e( 'Resume Migration', 'foogallery-migrate' ); ?></button>
+                            class="button button-primary continue_album_migrate"><?php esc_html_e( 'Resume Migration', 'foogallery-migrate' ); ?></button>
                     <button name="foogallery_migrate_action" value="foogallery_album_migrate_cancel"
-                            class="button cancel_album_migrate"><?php _e( 'Stop Migration', 'foogallery-migrate' ); ?></button>
+                            class="button cancel_album_migrate"><?php esc_html_e( 'Stop Migration', 'foogallery-migrate' ); ?></button>
                 <?php } else { ?>
                     <button name="foogallery_migrate_action" value="foogallery_album_migrate_start"
-                            class="button button-primary start_album_migrate"><?php _e( 'Start Album Migration', 'foogallery-migrate' ); ?></button>
+                            class="button button-primary start_album_migrate"><?php esc_html_e( 'Start Album Migration', 'foogallery-migrate' ); ?></button>
                 <?php
                 }
             }
             if ( $show_refresh ) { ?>
                 <button name="foogallery_migrate_action" value="foogallery_refresh_albums"
-                        class="button refresh_albums"><?php _e( 'Refresh Albums', 'foogallery-migrate' ); ?></button>
+                        class="button refresh_albums"><?php esc_html_e( 'Refresh Albums', 'foogallery-migrate' ); ?></button>
             <?php }
             ?><div id="foogallery_migrate_album_spinner" style="width:20px">
                 <span class="spinner"></span>
